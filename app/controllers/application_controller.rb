b5fb2b439cfb8ec2efc10e2923980615fd2ab3cd
Greg Haehnlein <greghaehnlein@gmail.com>
2017-10-09 10:59:54 -0400

    added gems, tweet resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_premiteed_parameters, if: :devise_controller?
+
+protected
+
+	def configure_premiteed_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :bio, :location])
+
+	def configure_premiteed_parameters
+		devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :bio, :location])
+	
+end
+
 end
